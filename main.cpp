4737fc99ff04106c95febf4252c53575987d54f1
stgatilov <stgatilov@localhost>
2018-08-06 00:42:02 +0700

    Refactored commands, added -file/-url key.

--- main.cpp
+++ main.cpp
@@ -5,104 +5,171 @@
 
 #ifdef WITH_CURL
 #include <curl/curl.h>
+CURL *curl = nullptr;
 #endif
 
 using namespace TdmSync;
 
 void exit_usage() {
-    fprintf(stderr, "Wrong arguments!");
+    fprintf(stderr, "Usage: \n");
+    fprintf(stderr, "  tdmsync prepare [file_path] (block_size=4096)\n");
+    fprintf(stderr, "    takes local file at [file_path] and preprocess it\n");
+    fprintf(stderr, "    saves metainformation into file [file_path].tdmsync\n");
+    fprintf(stderr, "    optional parameter [block_size] specified granularity of updates\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "  tdmsync update -file [source_file_path] [dest_file_path]\n");
+    fprintf(stderr, "    takes local file at [source_file_path] with metainformation at [source_file_path].tdmsync\n");
+    fprintf(stderr, "    synchronizes the local file at [dest_file_path] with it\n");
+    fprintf(stderr, "\n");
+#ifdef WITH_CURL
+    fprintf(stderr, "  tdmsync update -url [source_file_url] [dest_file_path]\n");
+    fprintf(stderr, "    takes remote file at [source_file_url] with metainformation at [source_file_url].tdmsync\n");
+    fprintf(stderr, "    synchronizes the local file at [dest_file_path] with it, downloading only metainfo and some parts of source\n");
+    fprintf(stderr, "\n");
+#endif
     exit(1);
 }
 
-int main(int argc, char **argv) {
-    if (argc < 3)
-        exit_usage();
+std::vector<std::string> arguments;
 
-    bool prepare;
-    if (strcmp(argv[1], "prepare") == 0) {
-        prepare = true;
+void commandPrepare() {
+    if (arguments.size() < 2) {
+        fprintf(stderr, "Prepare: missing file path argument\n\n");
+        exit_usage();
     }
-    else if (strcmp(argv[1], "update") == 0) {
-        prepare = false;
+
+    std::string dataFn = arguments[1];
+    std::string metaFn = dataFn + ".tdmsync";
+    fprintf(stderr, "Writing metainfo of file %s into file %s\n", dataFn.c_str(), metaFn.c_str());
+
+    int blockSize;
+    if (!(arguments.size() >= 3 && sscanf(arguments[2].c_str(), "%d", &blockSize) == 1))
+        blockSize = 4096;
+    fprintf(stderr, "Block size: %d\n", blockSize);
+
+    int starttime = clock();
+    //===========================================
+
+    StdioFile dataFile;
+    dataFile.open(dataFn.c_str(), StdioFile::Read);
+    FileInfo info;
+    info.computeFromFile(dataFile, blockSize);
+
+    StdioFile metaFile;
+    metaFile.open(metaFn.c_str(), StdioFile::Write);
+    info.serialize(metaFile);
+
+    //===========================================
+    int deltatime = clock() - starttime;
+    printf("Finished in %0.2lf sec\n", double(deltatime) / CLOCKS_PER_SEC);
+}
+
+void commandUpdate() {
+    if (arguments.size() < 4) {
+        fprintf(stderr, "Update: missing type, source or destination argument\n\n");
+        exit_usage();
     }
+
+    bool isLocal;
+    std::string type = arguments[1];
+    if (type == "-file")
+        isLocal = true;
+    else if (type == "-url")
+        isLocal = false;
     else {
+        fprintf(stderr, "Wrong type of update \"%s\"\n\n", type.c_str());
+        exit_usage();
+    }
+
+    std::string dataFn = arguments[2];
+    std::string metaFn = dataFn + ".tdmsync";
+    std::string localFn = arguments[3];
+    std::string downFn = localFn + ".download";
+    std::string resultFn = localFn + ".updated";
+
+    fprintf(stderr, "Updating local file from %s file:\n", (isLocal ? "local" : "remote"));
+    fprintf(stderr, "  %-40s  : local file to be updated\n", localFn.c_str());
+    fprintf(stderr, "  %-40s  : updated version of local file\n", resultFn.c_str());
+    fprintf(stderr, "  %-40s  : source file to be synchronized with\n", dataFn.c_str());
+    fprintf(stderr, "  %-40s  : file with metainformation to be read\n", metaFn.c_str());
+    fprintf(stderr, "  %-40s  : data downloaded from source file\n", downFn.c_str());
+
+    int starttime = clock();
+    //=======================================
+
+    #ifdef WITH_CURL
+    /*FILE *fp = fopen("downloaded.txt", "wb");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8000/tdm_ai_humanoid_females01.pk4.tdmsync");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    curl_easy_setopt(curl, CURLOPT_RANGE, "0-100,200-300");
+    int res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    fclose(fp);
+    exit(0);*/
+    #endif
+    
+    StdioFile metaFile;
+    metaFile.open(metaFn.c_str(), StdioFile::Read);
+    FileInfo info;
+    info.deserialize(metaFile);
+
+    int analysis_starttime = clock();
+    StdioFile localFile;
+    localFile.open(localFn.c_str(), StdioFile::Read);
+    UpdatePlan plan = info.createUpdatePlan(localFile);
+    plan.print();
+    printf("Analysis took %0.2lf sec\n", double(clock() - analysis_starttime) / CLOCKS_PER_SEC);
+    
+    StdioFile remoteFile;
+    remoteFile.open(dataFn.c_str(), StdioFile::Read);
+    StdioFile downloadFile;
+    downloadFile.open(downFn.c_str(), StdioFile::Write);
+    plan.createDownloadFile(remoteFile, downloadFile);
+    downloadFile.open(downFn.c_str(), StdioFile::Read);
+    
+    StdioFile resultFile;
+    resultFile.open(resultFn.c_str(), StdioFile::Write);
+    plan.apply(localFile, downloadFile, resultFile);
+
+    //===========================================
+    int deltatime = clock() - starttime;
+    printf("Finished in %0.2lf sec\n", double(deltatime) / CLOCKS_PER_SEC);
+}
+
+int main(int argc, char **argv) {
+    for (int i = 1; i < argc; i++)
+        arguments.push_back(argv[i]);
+    if (arguments.size() < 1) {
+        fprintf(stderr, "Command not specified\n\n");
         exit_usage();
     }
 
     #ifdef WITH_CURL
     curl_global_init(CURL_GLOBAL_DEFAULT);
-    CURL *curl = curl_easy_init();
+    curl = curl_easy_init();
     if (!curl) {
-        fprintf(stderr, "Failed to initialize curl");
+        fprintf(stderr, "Failed to initialize curl\n");
         exit(2);
     }
     #endif
 
-    std::string dataFn = argv[2];
-    std::string metaFn = dataFn + ".tdmsync";
-    std::string downFn = dataFn + ".download";
-
-    int starttime = clock();
     try {
-        if (prepare) {
-            int blockSize = argc >= 4 ? atoi(argv[3]) : 4096;
-
-            StdioFile dataFile;
-            dataFile.open(dataFn.c_str(), StdioFile::Read);
-            FileInfo info;
-            info.computeFromFile(dataFile, blockSize);
-
-            StdioFile metaFile;
-            metaFile.open(metaFn.c_str(), StdioFile::Write);
-            info.serialize(metaFile);
+        if (arguments[0] == "prepare") {
+            commandPrepare();
+        }
+        else if (arguments[0] == "update") {
+            commandUpdate();
         }
         else {
-            if (argc < 4) exit_usage();
-            std::string localFn = argv[3];
-
-            #ifdef WITH_CURL
-            FILE *fp = fopen("downloaded.txt", "wb");
-            curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8000/tdm_ai_humanoid_females01.pk4.tdmsync");
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-            curl_easy_setopt(curl, CURLOPT_RANGE, "0-100,200-300");
-            int res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-            fclose(fp);
-            exit(0);
-            #endif
-
-            StdioFile metaFile;
-            metaFile.open(metaFn.c_str(), StdioFile::Read);
-            FileInfo info;
-            info.deserialize(metaFile);
-
-            StdioFile localFile;
-            localFile.open(localFn.c_str(), StdioFile::Read);
-            auto plan = info.createUpdatePlan(localFile);
-            plan.print();
-            printf("Analysis tool %0.2lf sec\n", double(clock() - starttime) / CLOCKS_PER_SEC);
-
-            StdioFile remoteFile;
-            remoteFile.open(dataFn.c_str(), StdioFile::Read);
-            StdioFile downloadFile;
-            downloadFile.open(downFn.c_str(), StdioFile::Write);
-            plan.createDownloadFile(remoteFile, downloadFile);
-            downloadFile.open(downFn.c_str(), StdioFile::Read);
-
-            std::string resultFn = localFn + ".updated";
-            StdioFile resultFile;
-            resultFile.open(resultFn.c_str(), StdioFile::Write);
-            plan.apply(localFile, downloadFile, resultFile);
+            fprintf(stderr, "Unknown command \"%s\"\n\n", arguments[0].c_str());
+            exit_usage();
         }
     }
     catch(const std::exception &e) {
         printf("Exception!\n");
         printf("%s\n", e.what());
     }
-    int deltatime = clock() - starttime;
-
-    printf("Finished in %0.2lf sec\n", double(deltatime) / CLOCKS_PER_SEC);
 
     return 0;
 }
